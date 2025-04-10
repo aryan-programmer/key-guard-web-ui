7df64378ea2348423e00d9d23928480575057553
Aryan Chudasama <aryan.programer@gmail.com>
2025-04-10 20:56:30 +0530

    Prevent username & password auto complete.

--- src/lib/login-form.svelte
+++ src/lib/login-form.svelte
@@ -1,13 +1,20 @@
-<script lang="ts" module>
+<script lang="ts">
+	import { Control, Field, FieldErrors, Label } from "formsnap";
+	import { superForm } from "sveltekit-superforms";
+	import { zodClient } from "sveltekit-superforms/adapters";
 	import z, { type TypeOf } from "zod";
+	import { randomAddress } from "./uniq-address";
+
+	const usernameField = randomAddress();
+	const passwordField = randomAddress();
 	const schema = z.object({
-		username: z
+		[usernameField]: z
 			.string({
 				invalid_type_error: "Enter a valid user name",
 				required_error: "User name is required"
 			})
 			.nonempty("User name is required"),
-		password: z
+		[passwordField]: z
 			.string({
 				invalid_type_error: "Enter a valid password",
 				required_error: "Password is required"
@@ -15,18 +22,12 @@
 			.nonempty("Password is required")
 	});
 	type Data_T = TypeOf<typeof schema>;
-</script>
 
-<script lang="ts">
-	import { Control, Field, FieldErrors, Label } from "formsnap";
-	import { superForm } from "sveltekit-superforms";
-	import { zodClient } from "sveltekit-superforms/adapters";
-
-	let { onSubmit }: { onSubmit: (data: Data_T) => void } = $props();
+	let { onSubmit }: { onSubmit: (data: { username: string; password: string }) => void } = $props();
 	const form = superForm<Data_T>(
 		{
-			username: "",
-			password: ""
+			[usernameField]: "",
+			[passwordField]: ""
 		},
 		{
 			validators: zodClient(schema)
@@ -34,40 +35,54 @@
 	);
 	const { form: formData, validateForm, errors } = form;
 
+	let readonlyUsername = $state(true);
+	let readonlyPassword = $state(true);
+
 	async function handleSubmit(event: Event) {
-		event.preventDefault();
 		const result = await validateForm();
+
+		console.log(result);
 
 		if (!result.valid) {
 			errors.update((v) => {
 				return {
 					...v,
-					username: result.errors.username,
-					password: result.errors.password
+					[usernameField]: result.errors[usernameField],
+					[passwordField]: result.errors[passwordField]
 				};
 			});
 
 			return;
 		}
-		onSubmit(result.data);
+		onSubmit({
+			username: result.data[usernameField],
+			password: result.data[passwordField]
+		});
 	}
 </script>
 
-<form
-	onsubmit={handleSubmit}
-	class="background-blank m-2 grid grid-cols-[min-content_1fr] items-center overflow-hidden"
->
-	<Field {form} name="username">
-		<Control>
+<div class="background-blank m-2 grid grid-cols-[min-content_1fr] items-center overflow-hidden">
+	<input
+		type="password"
+		id="prevent-autofill"
+		autocomplete="off"
+		style="display: none"
+		tabindex="-1"
+	/>
+	<Field {form} name={usernameField}>
+		<Control id={randomAddress()}>
 			{#snippet children({ props })}
 				<Label>User&nbsp;Name</Label>
 				<div>
 					<div class="flex w-full flex-row justify-stretch">
 						<input
 							class="input neob-focus"
+							autocomplete="off"
 							{...props}
 							type="text"
-							bind:value={$formData.username}
+							bind:value={$formData[usernameField]}
+							readonly={readonlyUsername}
+							onfocus={() => (readonlyUsername = false)}
 						/>
 					</div>
 				</div>
@@ -76,8 +91,8 @@
 		<div></div>
 		<FieldErrors class="m-with-shadow !my-0" />
 	</Field>
-	<Field {form} name="password">
-		<Control>
+	<Field {form} name={passwordField}>
+		<Control id={randomAddress()}>
 			{#snippet children({ props })}
 				<Label>Password</Label>
 				<div>
@@ -85,8 +100,12 @@
 						<input
 							class="input neob-focus w-full"
 							{...props}
-							type="password"
-							bind:value={$formData.password}
+							type="text"
+							style="text-security: disc; -webkit-text-security: disc;"
+							autocomplete="off"
+							bind:value={$formData[passwordField]}
+							readonly={readonlyPassword}
+							onfocus={() => (readonlyPassword = false)}
 						/>
 					</div>
 				</div>
@@ -96,7 +115,7 @@
 		<FieldErrors class="m-with-shadow !my-0" />
 	</Field>
 	<div></div>
-	<button class="button neob-hover button-size-default background-main" type="submit">
+	<button class="button neob-hover button-size-default background-main" onclick={handleSubmit}>
 		Login
 	</button>
-</form>
+</div>
